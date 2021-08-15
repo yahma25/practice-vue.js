e99060803d5904f27f6773e7cadf8cb9d521facf
mhkim <yahma25@gmail.com>
2021-08-15 18:37:34 +0900

    vue-testing-library에서 제공하는 샘플 추가

---
+++ __tests__/basic/Button.test.js
@@ -0,0 +1,32 @@
+import { render, fireEvent } from '@testing-library/vue';
+import Button from './Button.vue';
+import '@testing-library/jest-dom';
+
+test('renders button with text', () => {
+  const text = "Click me; I'm sick";
+
+  // Set the prop value by using the second argument of `render()`.
+  const { getByRole } = render(Button, {
+    props: { text },
+  });
+
+  // Get the only element with a 'button' role.
+  const button = getByRole('button');
+
+  expect(button).toHaveTextContent(text);
+});
+
+test('emits click event when button is clicked', async () => {
+  const text = 'Click me';
+
+  const { getByRole, emitted } = render(Button, {
+    props: { text },
+  });
+
+  // Send a click event.
+  await fireEvent.click(getByRole('button'));
+
+  // Expect that the event emitted a "click" event. We should test for emitted
+  // events has they are part of the public API of the component.
+  expect(emitted()).toHaveProperty('click');
+});
