b70cdafe374b373a776dc10f469d1984d513ccdc
mhkim <yahma25@gmail.com>
2021-08-25 00:20:00 +0900

    라우터를 추가하여 샘플 페이지가 경로에 따라 보여지도록 함

---
+++ projects/vue-news/src/router/index.js
@@ -0,0 +1,24 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import NewsView from '../views/NewsView.vue';
+import JobsView from '../views/JobsView.vue';
+import AskView from '../views/AskView.vue';
+
+Vue.use(VueRouter);
+
+export const router = new VueRouter({
+  routes: [
+    {
+      path: '/news',
+      component: NewsView
+    },
+    {
+      path: '/jobs',
+      component: JobsView
+    },
+    {
+      path: '/ask',
+      component: AskView
+    }
+  ]
+});
---
+++ projects/vue-news/src/views/NewsView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>news</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
