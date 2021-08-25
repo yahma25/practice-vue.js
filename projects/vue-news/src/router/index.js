b9945047e230ff235d4778ee2fb0617943bd881d
mhkim <yahma25@gmail.com>
2021-08-25 22:59:59 +0900

    루트 경로는 뉴스 페이지가 보이도록 변경

--- projects/vue-news/src/router/index.js
+++ projects/vue-news/src/router/index.js
@@ -9,6 +9,10 @@ Vue.use(VueRouter);
 export const router = new VueRouter({
   routes: [
     {
+      path: '/',
+      redirect: '/news'
+    },
+    {
       path: '/news',
       component: NewsView
     },
