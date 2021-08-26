d399d6af0376f5deed7bcb3e804ffcd2c8713c7f
mhkim <yahma25@gmail.com>
2021-08-26 23:03:13 +0900

    jest 실행에 필요한 환경 설정 추가

--- projects/vue-news/babel.config.js
+++ projects/vue-news/babel.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  presets: [
-    '@vue/cli-plugin-babel/preset'
-  ]
-}
+  // presets: ['@vue/cli-plugin-babel/preset']
+  // '@vue/cli-plugin-babel/preset'를 사용하지 않는다면 async/await을 사용하기 위해 추가해야 함
+  presets: ['@babel/preset-env'],
+  plugins: ['@babel/plugin-transform-runtime']
+};
