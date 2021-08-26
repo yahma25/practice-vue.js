49af5fcbb1e9f91a73bb56d2ae9be96271b8065f
mhkim <yahma25@gmail.com>
2021-08-27 00:18:54 +0900

    parsing 오류가 발생하던 문제 해결

--- projects/vue-news/.eslintrc.js
+++ projects/vue-news/.eslintrc.js
@@ -1,6 +1,8 @@
 module.exports = {
   // "Parsing error: The keyword 'import' is reserved" 오류 해결
-  parser: 'babel-eslint',
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
   // jest 테스트 코드도 대상에 포함시키도록 함
   env: {
     jest: true
