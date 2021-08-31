8738966330ff9b6ecd2625f29d888ebaadf4a9da
mhkim <yahma25@gmail.com>
2021-08-31 12:45:40 +0900

    문서화를 위해 Sample 명칭은 제거

--- projects/vue-news/__tests__/sample/router/App.spec.js
+++ projects/vue-news/__tests__/sample/router/App.spec.js
@@ -1,7 +1,7 @@
 import { mount, createLocalVue } from '@vue/test-utils';
 import VueRouter from 'vue-router';
-import App from './SampleApp.vue';
-import Main from './SampleMain.vue';
+import App from './App.vue';
+import Main from './MainView.vue';
 
 const localVue = createLocalVue();
 localVue.use(VueRouter);
