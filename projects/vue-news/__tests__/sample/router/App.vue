b72264d18e4bf7158bdc362c313ea9adf2973b19
mhkim <yahma25@gmail.com>
2021-09-01 23:49:38 +0900

    nav 추가 + css style

--- projects/vue-news/__tests__/sample/router/App.vue
+++ projects/vue-news/__tests__/sample/router/App.vue
@@ -1,6 +1,13 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+    <nav class="nav_bar">
+      <router-link to="/main">Main</router-link>
+      <span class="seperator">|</span>
+      <router-link to="/board">Board</router-link>
+    </nav>
+    <section>
+      <router-view></router-view>
+    </section>
   </div>
 </template>
 
@@ -9,3 +16,18 @@ export default {
   name: 'App'
 }
 </script>
+
+<style scope>
+  .nav_bar {
+    display: flex;
+    align-content: center;
+    justify-content: center;
+    color: white;
+    background-color: #42b883;
+    padding: 8px;
+  }
+
+  .seperator {
+    padding: 0 4px;
+  }
+</style>
