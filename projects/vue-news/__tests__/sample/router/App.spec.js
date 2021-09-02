71d87b65bb306f851b7fcad28b4e55c138d750fc
mhkim <yahma25@gmail.com>
2021-09-02 21:20:04 +0900

    Board 뷰 라우트 테스트 케이스 추가

--- projects/vue-news/__tests__/sample/router/App.spec.js
+++ projects/vue-news/__tests__/sample/router/App.spec.js
@@ -2,13 +2,14 @@ import { mount, createLocalVue } from '@vue/test-utils';
 import VueRouter from 'vue-router';
 import App from './App.vue';
 import Main from './views/MainView.vue';
+import Board from './views/BoardView.vue';
 import router from './router';
 
 const localVue = createLocalVue();
 localVue.use(VueRouter);
 
 describe('App', () => {
-  it('renders route components', async () => {
+  it('renders main route component', async () => {
     const wrapper = mount(App, {
       localVue,
       router
@@ -18,5 +19,17 @@ describe('App', () => {
     await wrapper.vm.$nextTick();
 
     expect(wrapper.findComponent(Main).exists()).toBe(true);
+  });
+
+  it('renders board component', async () => {
+    const wrapper = mount(App, {
+      localVue,
+      router
+    });
+
+    router.push('/board');
+    await wrapper.vm.$nextTick();
+
+    expect(wrapper.findComponent(Board).exists()).toBe(true);
   });
 });
