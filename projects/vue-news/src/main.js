3b18c91b87e86d833badfd304c824c68b96b540d
mhkim <yahma25@gmail.com>
2021-11-06 23:48:10 +0900

    fix: Change app vue target
    Co-authored-by: Dongwoo Kim <dngwoodo@users.noreply.github.com>

--- projects/vue-news/src/main.js
+++ projects/vue-news/src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue';
-import App from './App.vue';
-import { router } from './routes';
+// import App from './App.vue';
+// import { router } from './routes';
+// import App from '../__tests__/sample/router/App.vue';
+import App from '../TodoApp/App.vue';
+import router from '../__tests__/sample/router/router';
 
 Vue.config.productionTip = false;
 
